4a99d68df3e22b440379de76e83813ef11f9ae26
Shahd1819 <75165406+Shahd1819@users.noreply.github.com>
2022-04-15 23:38:27 +0200

    Update Database.py

--- Database/Database.py
+++ Database/Database.py
@@ -4,5 +4,5 @@ try:
     client = MongoClient("mongodb+srv://karimhafez:KojGCyxxTJXTYKYV@cluster0.buuqk.mongodb.net/admin")
     Database = client.Twitter_new
 except: 
-    print("can't connect")
+    print("Error: Cannot connect to Database")
 
