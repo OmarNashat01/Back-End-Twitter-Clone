7664e0506a3d29af391f292e863d86e5c4b12dd3
kikohafez78 <76871285+kikohafez78@users.noreply.github.com>
2022-05-22 20:38:41 +0200

    final update

---
+++ Routes/Tweets/search.py
@@ -0,0 +1,217 @@
+from Routes.Tweetstruct import Tweet, collectionoftweets, token_required, col_of_users, retweet, col_of_tweets, objectid_of_like_dates, col_of_stats, comment, collectionofcomments, Client, collectionofretweets, saveimages
+from flask import Blueprint, request, jsonify, send_file
+from flask_cors import cross_origin
+from datetime import datetime, timedelta
+from bson import ObjectId
+import operator
+import pymongo
+import numpy
+
+
+Home_app = Blueprint("Home_app", __name__)
+
+
+@Home_app.route("/random", methods=["GET"])
+@cross_origin(allow_headers=['Content-Type', 'x-access-token', 'Authorization'])
+@token_required
+def get_all_tweets(current_user):
+    # number of tweets to return
+    pag_token = request.args.get("page", default=1, type=int)
+    tweets = collectionoftweets()
+    retweets = collectionofretweets()
+    if tweets.Tweets != []:
+        tweets.Tweets = []
+    t = tweets.get_random_from_database(int(pag_token))
+    t1 = retweets.get_random_from_database(int(pag_token))
+    if pag_token <= 0 or int(pag_token) is False:
+        return {"400": "invalid pagination token,please enter an integer number above 0"}, 400
+    elif t is True and t1 is True:
+        return {"404": "tweets and retweets are unavailable"}, 404
+    tweets.Tweets += retweets.retweets
+    tweets.Tweets.sort(key=operator.itemgetter("created_at"),reverse = True)
+    if pag_token > len(list(tweets.Tweets)):
+        return {"all_tweets_retweets": tweets.Tweets}, 201
+    return {"all_tweets_retweets": tweets.Tweets}, 200
+
+
+@Home_app.route("/all/me", methods=["GET"])
+@cross_origin(allow_headers=['Content-Type', 'x-access-token', 'Authorization'])
+@token_required
+def get_all_user_tweets_tweets(current_user):
+    # number of tweets to return
+    pag_token = 1
+    _id = current_user["_id"]
+    tweets = collectionoftweets()
+    retweets = collectionofretweets()
+    if tweets.Tweets != []:
+        tweets.Tweets = []
+    t = tweets.get_from_user_tweets_database(int(pag_token), ObjectId(_id))
+    t1 = retweets.get_from_user_tweets_database(int(pag_token), ObjectId(_id))
+    if pag_token <= 0 or int(pag_token) is False:
+        return {"400": "invalid pagination token,please enter an integer number above 0"}, 400
+    elif t is True and t1 is True:
+        return {"404": "tweets and retweets are unavailable"}, 404
+    tweets.Tweets += retweets.retweets
+    tweets.Tweets.sort(key=operator.itemgetter("created_at"),reverse = True)
+    if pag_token > len(list(tweets.Tweets)):
+        return {"all_tweets_retweets": tweets.Tweets}, 201
+
+    return {"all_tweets_retweets": tweets.Tweets}, 200
+
+
+@Home_app.route("/all", methods=["GET"])
+@cross_origin(allow_headers=['Content-Type', 'x-access-token', 'Authorization'])
+@token_required
+def get_all_user_tweets(current_user):
+    # number of tweets to return
+    Id = str(request.args.get("Id", default=None, type=str))
+    pag_token = 1
+    try:
+        _id = ObjectId(Id)
+    except:
+        return {"400": "Invalid ID"}, 400
+    tweets = collectionoftweets()
+    retweets = collectionofretweets()
+    if tweets.Tweets != []:
+        tweets.Tweets = []
+        retweets.retweets = [] 
+    t = tweets.get_from_user_tweets_database(int(pag_token), ObjectId(_id))
+    t1 = retweets.get_from_user_tweets_database(int(pag_token), ObjectId(_id))
+    if pag_token <= 0 or int(pag_token) is False:
+        return {"400": "invalid pagination token,please enter an integer number above 0"}, 400
+    elif t is True and t1 is True:
+        return {"404": "tweets and retweets are unavailable"}, 404
+    tweets.Tweets += retweets.retweets
+    tweets.Tweets.sort(key=operator.itemgetter("created_at"), reverse=True)
+    if pag_token > len(list(tweets.Tweets)):
+        return {"all_tweets_retweets": tweets.Tweets}, 201
+
+    return {"all_tweets_retweets": tweets.Tweets}, 200
+
+
+@Home_app.route("/all/followings", methods=["GET"])
+@cross_origin(allow_headers=['Content-Type', 'x-access-token', 'Authorization'])
+@token_required
+def get_all_followings_tweets(current_user):
+    # number of tweets to return
+    Id = str(request.args.get("Id", default=None, type=str))
+    pag_token = 1
+    try:
+        _id = ObjectId(Id)
+    except:
+        return {"400": "Invalid ID"}, 400
+    tweets = collectionoftweets()
+    retweets = collectionofretweets()
+    if tweets.Tweets != []:
+        tweets.Tweets = []
+    t = tweets.get_from_followings_tweets_database(int(pag_token), _id)
+    t1 = tweets.get_from_followings_tweets_database(int(pag_token),_id)
+    if pag_token <= 0 or int(pag_token) is False:
+        return {"400": "invalid pagination token,please enter an integer number above 0"}, 400
+    elif t is True and t1 is True:
+        return {"404": "liked tweets are unavailable"}, 404
+    tweets.Tweets += retweets.retweets
+    tweets.Tweets.sort(key=operator.itemgetter("created_at"), reverse=True)
+    if pag_token > len(list(tweets.Tweets)):
+        return {"all_tweets_retweets": tweets.Tweets}, 201
+
+    return {"all_tweets_retweets": tweets.Tweets}, 200
+
+
+@Home_app.route("/search/all", methods=["GET"])
+@cross_origin(allow_headers=['Content-Type', 'x-access-token', 'Authorization'])
+@token_required
+def get_all_query_retweets(current_user):
+    # number of tweets to return
+
+    pag_token = 1
+    try:
+        string = str(request.args.get("q", default=None, type=str))
+        q = string.lower()
+    except:
+        return {"400": "Invalid query"}, 400
+    tweets = collectionofretweets()
+    retweets = collectionoftweets()
+    print(str(q))
+    if tweets.retweets != []:
+        tweets.retweets = []
+    t = tweets.get_query_from_database(1, q)
+    t1 = retweets.get_query_from_database(1,q)
+    if pag_token <= 0 or int(pag_token) is False:
+        return {"400": "invalid pagination token,please enter an integer number above 0"}, 400
+    elif t is True and t1 is True:
+        return {"404": "tweets and retweets are unavailable"}, 404
+    tweets.retweets += retweets.Tweets
+    tweets.retweets.sort(key=operator.itemgetter("created_at"), reverse=True)
+    if pag_token > len(list(tweets.retweets)):
+        return {"all_tweets_retweets": tweets.retweets}, 201
+
+    return {"all_tweets_retweets": tweets.retweets}, 200
+
+
+@Home_app.route("/search/recent", methods=["GET"])
+@cross_origin(allow_headers=['Content-Type', 'x-access-token', 'Authorization'])
+@token_required
+def get_all_query_recent_retweets(current_user):
+    # number of tweets to return
+
+    pag_token = 1
+    try:
+        string = str(request.args.get("q", default=None, type=str))
+        q = string.lower()
+    except:
+        return {"400": "Invalid query"}, 400
+    tweets = collectionofretweets()
+    retweets = collectionoftweets()
+    if tweets.retweets != []:
+        tweets.retweets = []
+    t = tweets.get_query_from_database(1, q)
+    t1 = retweets.get_query_from_database(1, q)
+    if pag_token <= 0 or int(pag_token) is False:
+        return {"400": "invalid pagination token,please enter an integer number above 0"}, 400
+    elif t is True and t1 is True:
+        return {"404": "tweets and retweets are unavailable"}, 404
+    tweets.retweets += retweets.Tweets
+    tweets.retweets.sort(key=operator.itemgetter("created_at"), reverse=True)
+    today = (datetime.strptime(
+        (datetime.now()-timedelta(days=7)).strftime("%Y-%m-%d"), '%Y-%m-%d'))
+    for tweet1 in list(tweets.retweets):
+        if datetime.strptime(tweet1["created_at"], '%Y-%m-%d') > today:
+            print(tweet1["created_at"])
+        else:
+            tweets.retweets.remove(tweet1)
+    if tweets.retweets == []:
+        return {"404": "tweets and retweets are unavailable"}, 404
+    if pag_token > len(list(tweets.retweets)):
+        return {"recent_tweets_retweets": tweets.retweets}, 201
+
+    return {"recent_tweets_retweets": tweets.retweets}, 200
+
+
+@Home_app.route("/all/liked", methods=["GET"])
+@cross_origin(allow_headers=['Content-Type', 'x-access-token', 'Authorization'])
+@token_required
+def get_all_liked_tweets(current_user):
+    # number of tweets to return
+    Id = str(request.args.get("Id", default=None, type=str))
+    pag_token = 1
+    try:
+        _id = ObjectId(Id)
+    except:
+        return {"400": "Invalid ID"}, 400
+    tweets = collectionoftweets()
+    retweets = collectionofretweets()
+    if tweets.Tweets != []:
+        tweets.Tweets = []
+    t = tweets.get_from_liked_tweets_database(1, _id)
+    t1 = retweets.get_from_liked_tweets_database(1, _id)
+    if pag_token <= 0 or int(pag_token) is False:
+        return {"400": "invalid pagination token,please enter an integer number above 0"}, 400
+    elif t is True and t1 is True:
+        return {"404": "liked tweets and retweets are unavailable"}, 404
+    tweets.Tweets += retweets.retweets
+    tweets.Tweets.sort(key=operator.itemgetter("created_at"), reverse=True)
+    if pag_token > len(list(tweets.Tweets)):
+        return {"liked_tweets_retweets": tweets.Tweets}, 201
+
+    return {"liked_tweets_retweets": tweets.Tweets}, 200
--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
-
-# Importing all Routes
+#Importing all Routes
 
 from Routes.Friendships.followers import followers
 from Routes.Friendships.following import following
@@ -16,45 +18,34 @@ from Routes.Signup.signup import signup, mail
 from Routes.get_me.get_me import get_me
 from Routes.get_user.get_user import get_user
 from Routes.update_user.update_user import update_user
-from Routes.block_user.post_block import block
-from Routes.block_user.get_blocked import getblock
-from Routes.get_blocked_user.get_blocked_users import get_blocked_users
-from Routes.unblock_user.unblock_user import unblock_user
-from Routes.change_password.change_password import change_password
-from Routes.forgot_password.forgot_password import forgot_password
-
+from Routes.Tweets.extra import Tweet_app1
+from Routes.Tweets.search import Home_app
 
 # Creating flask application
 app = Flask(__name__, template_folder='Templates')
 app.config.from_pyfile('config.cfg')
 mail.init_app(app)
+app.config['UPLOAD_FOLDER'] = "E:\Back-End-Twitter-Clone\photos"
+
+
 
 
 # Registering all the blue prints created in other files
 app.register_blueprint(Login, url_prefix='/Login')
 app.register_blueprint(GetAll, url_prefix='/users')
 app.register_blueprint(signup, url_prefix='/signup')
-
+app.register_blueprint(Home_app,url_prefix='/home')
 app.register_blueprint(followers, url_prefix='/users')
 app.register_blueprint(following, url_prefix='/users')
 app.register_blueprint(new_accounts_count, url_prefix='/admin/statistics')
 app.register_blueprint(retweets_count, url_prefix='/admin/statistics')
 app.register_blueprint(Tweet_app, url_prefix='/tweets')
+app.register_blueprint(Tweet_app1, url_prefix='/users')
 app.register_blueprint(Tweet_stats, url_prefix='/admin/statistics')
 
 app.register_blueprint(get_me, url_prefix='/users')
 app.register_blueprint(get_user, url_prefix='/users')
 app.register_blueprint(update_user, url_prefix='/users')
-app.register_blueprint(block, url_prefix='/admin')
-app.register_blueprint(getblock, url_prefix='/admin')
-
-app.register_blueprint(get_blocked_users, url_prefix='/users')
-app.register_blueprint(unblock_user, url_prefix='/users')
-app.register_blueprint(change_password, url_prefix='/users')
-app.register_blueprint(forgot_password, url_prefix='/users')
-
-
-app.secret_key = "CodeSpecialist.com"
 
 
 CORS(app)
